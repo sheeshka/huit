0ee2283ff9624aab060ab2b5f23b96e8cd67d55e
sheeshka <sheeshka@ya.ru>
2018-06-20 17:12:29 +0300

    Админ панель

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,7 @@ Route::get('/home', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['prefix'=>'admin', 'namespace'=>'Admin', 'middleware'=>['auth']], function (){
+    Route::get('/', 'DashboardController@dashboard') ->name('admin.index');
+});
