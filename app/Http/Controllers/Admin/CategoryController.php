4ee54e084dcb13cc0b9de523ef7d10b43d2160c6
sheeshka <sheeshka@ya.ru>
2018-06-20 20:23:02 +0300

    Закончил урок 7

---
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Category;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('admin.categories.index', [
+            'categories' => Category::paginate(10)
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Caregory  $caregory
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Caregory $caregory)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Caregory  $caregory
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Caregory $caregory)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Caregory  $caregory
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Caregory $caregory)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Caregory  $caregory
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Caregory $caregory)
+    {
+        //
+    }
+}
