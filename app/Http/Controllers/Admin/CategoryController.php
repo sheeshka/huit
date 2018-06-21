10ef2220514e5323ab97958957a6997fb3b98d47
sheeshka <sheeshka@ya.ru>
2018-06-21 21:22:43 +0300

    Закончил урок 9 точно

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -51,10 +51,10 @@ class CategoryController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  \App\Caregory  $caregory
+     * @param  \App\Category  $category
      * @return \Illuminate\Http\Response
      */
-    public function show(Caregory $caregory)
+    public function show(Category $category)
     {
         //
     }
@@ -62,33 +62,39 @@ class CategoryController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\Caregory  $caregory
+     * @param  \App\Category  $category
      * @return \Illuminate\Http\Response
      */
-    public function edit(Caregory $caregory)
+    public function edit(Category $category)
     {
-        //
+        return view('admin.categories.edit', [
+            'category'   => $category,
+            'categories' => Category::with('children')->where('parent_id', '0')->get(),
+            'delimiter'  => ''
+        ]);
     }
 
     /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Caregory  $caregory
+     * @param  \App\Category  $category
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Caregory $caregory)
+    public function update(Request $request, Category $category)
     {
-        //
+        $category->update($request->except('slug'));
+
+        return redirect()->route('admin.category.index');
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Caregory  $caregory
+     * @param  \App\Category  $category
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Caregory $caregory)
+    public function destroy(Category $category)
     {
         //
     }
---
+++ resources/views/admin/categories/edit.blade.php
@@ -0,0 +1,22 @@
+@extends('admin.layouts.app_admin')
+
+@section('content')
+
+    <div class="container">
+
+        @component('admin.components.breadcrumb')
+            @slot('title') Редактирование категории @endslot
+            @slot('parent') Главная @endslot
+            @slot('active') Категории @endslot
+            @endcomponent
+
+            </hr>
+
+            <form action="{{route('admin.category.update', $category)}}" class="form-horizontal" method="post">
+                <input type="hidden" name="_method" value="put">
+                {{ csrf_field() }}
+                @include('admin.categories.partials.form')
+            </form>
+    </div>
+
+@endsection
